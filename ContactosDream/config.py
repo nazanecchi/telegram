05db542bd93f0a702d347d1b6fb8875c048ae8e4
nazanecchi <62289237+nazanecchi@users.noreply.github.com>
2024-08-21 16:12:00 -0300

    Delete ContactosDream/config.py

--- ContactosDream/config.py
+++
@@ -1,4 +0,0 @@
-import telebot
-telegram_token = '5407338327:AAGbjmIDcnht43FljtK-H77TVGcsV4-J2XQ'
-ngrok_token = '2gNcPyhSzrgRdDMdlMi5oMNcaN0_3KMhEzrrjZ6P4qxw2dYrm'
-bot = telebot.TeleBot(telegram_token)
